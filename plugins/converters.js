073047646f5ae998945a4ff00d7a0fc558fd2040
•𝐋𝐎𝐑𝐃 𝐊𝐄𝐑𝐌• <kermd237@gmail.com>
2025-03-13 21:13:32 +0100

    Update converters.js

--- plugins/converters.js
+++ plugins/converters.js
@@ -1,12 +1,7 @@
 const { cmd } = require('../command');
 const config = require('../config');
-const axios = require('axios');
-const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, sleep, fetchJson, empiretourl } = require('../lib/functions');
-const ffmpeg = require('fluent-ffmpeg');
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
-const fs = require('fs');
-const path = require('path');
-const { sms, downloadMediaMessage } = require('../lib/msg');
+const path = require("path");
+const { exec } = require("child_process");
 
 cmd({
     pattern: "mp3",
