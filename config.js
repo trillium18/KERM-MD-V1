24b4f5e935a1288b34e7d1cc44bd96444c50b7f4
•𝐋𝐎𝐑𝐃 𝐊𝐄𝐑𝐌• <kermd237@gmail.com>
2025-03-14 08:55:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: require('..my_data/mode.json').mode || "private"
+MODE: require('..my_data/mode.json').mode || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
