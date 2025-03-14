547324d3841a1afc4b0ab8881c5f9a8934774ab0
•𝐋𝐎𝐑𝐃 𝐊𝐄𝐑𝐌• <kermd237@gmail.com>
2025-03-14 15:55:00 +0100

    Update opentime-closetime.js

--- plugins/opentime-closetime.js
+++ plugins/opentime-closetime.js
@@ -40,7 +40,7 @@ if (!isAdmins) return reply(ADMIN)
                 reply(`Open time ${q} starting from now`)
                 setTimeout(() => {
                     var nomor = mek.participant
-                    const open = `*OPEN TIME* THE GROUP WAS OPENED BY KERM-MD-V1 TO APPROVED ADMIN\n NOW MEMBERS CAN SEND MESSAGES 🔓`
+                    const open = `OPEN TIME THE GROUP WAS OPENED BY KERM-MD-V1 TO APPROVED ADMIN\n NOW MEMBERS CAN SEND MESSAGES 🔓`
                     conn.groupSettingUpdate(from, 'not_announcement')
                     reply(open)
                 }, timer)
@@ -77,7 +77,7 @@ if (!isAdmins) return reply(ADMIN)
                 reply(`Close time ${q} starting from now`)
                 setTimeout(() => {
                     var nomor = m.participant
-                    const close = `*CLOSE TIME* GROUP CLOSED BY KERM-MD-V1 AT APPROVED ADMIN\nNOW ONLY ADMIN CAN SEND MESSAGES 🔐`
+                    const close = `CLOSE TIME GROUP CLOSED BY KERM-MD-V1 AT APPROVED ADMIN\nNOW ONLY ADMIN CAN SEND MESSAGES 🔐`
                     conn.groupSettingUpdate(from, 'announcement')
                     reply(close)
                 }, timer)
