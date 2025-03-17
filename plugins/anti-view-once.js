b27d5579d571c07a67fc569f5cd3e5dd1095af11
•𝐋𝐎𝐑𝐃 𝐊𝐄𝐑𝐌• <kermd237@gmail.com>
2025-03-17 18:40:05 +0100

    Update anti-view-once.js

--- plugins/anti-view-once.js
+++ plugins/anti-view-once.js
@@ -14,7 +14,7 @@ Github: Kgtech-cmr
 const config = require('../config');
 const { cmd, commands } = require('../command');
 const { proto, downloadContentFromMessage } = require('@whiskeysockets/baileys');
-const { sms,downloadMediaMessage } = require('../Lib/msg');
+const { sms,downloadMediaMessage } = require('../lib/msg');
 const fs = require('fs');
 const exec = require('child_process');
 const path = require('path');
